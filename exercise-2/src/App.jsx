f0bf112250a9b90451a39c50bd561730da655464
Huỳnh Đức <duc.huynh25@student.passerellesnumeriques.org>
2024-06-08 11:02:43 +0700

    ex-2

--- exercise-2/src/App.jsx
+++ exercise-2/src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useState } from "react";
 function App() {
-  /* You will need to use a boolean state to keep the value of the text entered */
   const [currentValue, setCurrentValue] = useState("")
-  /* You will need a function to handle a key pressed on the first input and update the state*/
-  // Hàm xử lý khi người dùng nhấn phím
   function handelKey(e){
     const text = e.target.value;
     const value = text.toUpperCase();
@@ -21,7 +18,6 @@ function App() {
       <p>
         <label>Here is the text in upper case</label>
 
-        {/* This input will need to display the text entered in lower case */}
         <input disabled value={currentValue}/>
       </p>
     </main>
